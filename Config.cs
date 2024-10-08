61de26d3c68f536861aa120ab9676df37ae2bdeb
amacapozzi <amacapozzi07@gmail.com>
2024-10-08 12:54:38 -0300

    add BASE API URL

---
+++ Config.cs
@@ -0,0 +1,9 @@
+﻿namespace RegistryAnalyzer
+{
+    internal class Config
+    {
+        public static readonly string VIRUS_TOTAL_APIKEY = "";
+
+        public static readonly string BASE_API_URL = "https://www.virustotal.com/api/v3";
+    }
+}
